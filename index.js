b13eea46105bc002441e790c3c034e03fe77272a
Manoj R. Randeni <randeni.ramr@gmail.com>
2021-01-23 19:34:49 +0530

    Refine functions

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
+//get html elements
 const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
 const newJokeBtn = document.getElementById("newJokeBtn");
 
+newJokeBtn.addEventListener("click", getJoke);
+
+// Get a joke asyn
 async function getJoke() {
   const jokePromise = await fetch(
     "https://official-joke-api.appspot.com/jokes/programming/random"
   );
   if (jokePromise.ok) {
-    if (punchlineDiv.textContent) punchlineDiv.classList.add("hidden");
     const joke = await jokePromise.json();
+
     const { punchline, setup } = joke[0];
     setupDiv.textContent = setup;
-    setupDiv.addEventListener("click", () => {
-      punchlineDiv.textContent = punchline;
-      punchlineDiv.classList.add("bubble");
-      punchlineDiv.classList.remove("hidden");
-    });
-    // console.log(setup, punchline);
+    punchlineDiv.textContent = punchline;
+
+    punchlineDiv.classList.add("hidden");
+
+    toggleNewJokeAndPunchlineBtns();
   } else {
     console.error(`Error: status code ${jokePromise.status}`);
   }
 }
 getJoke();
-newJokeBtn.addEventListener("click", getJoke);
+
+punchlineBtn.addEventListener("click", getPunchline);
+
+// Joke punchline
+function getPunchline() {
+  punchlineDiv.classList.add("bubble");
+  punchlineDiv.classList.remove("hidden");
+
+  toggleNewJokeAndPunchlineBtns();
+}
+
+// toggle joke and punchline btns
+function toggleNewJokeAndPunchlineBtns() {
+  newJokeBtn.classList.toggle("hidden");
+  punchlineBtn.classList.toggle("hidden");
+}
